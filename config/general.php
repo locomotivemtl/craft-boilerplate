01e2adbe7e99610a4d7457b365c23627651bbf4a
Pascal Rioux <contact@pascalrioux.com>
2025-05-16 09:25:43 -0400

    Add quick way to toggle GraphQL

--- config/general.php
+++ config/general.php
@@ -21,6 +21,8 @@ return GeneralConfig::create()
     ->preloadSingles()
     // Prevent user enumeration attacks
     ->preventUserEnumeration()
+    // Disable GraphQL
+    //->enableGql(false)
     // Set the @webroot alias so the clear-caches command knows where to find CP resources
     ->aliases([
         '@webroot' => dirname(__DIR__) . '/web',
